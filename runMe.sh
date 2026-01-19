f0ae7d3fa6268c662e71a7492806f359585908dc
ferr <g.f.guimaraes@hotmail.com>
2026-01-19 16:47:56 -0300

    update script

--- runMe.sh
+++ runMe.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-cd frontend && mv .env.prod .env && npm install && npm run build
-cd ../backend && mv .env.prod .env && npm install && npm run build
+cd frontend && cp .env.prod .env && npm install && npm run build
+cd ../backend && cp .env.prod .env && npm install && npm run build
 
 mv ../frontend/dist ./frontend
 
+cd .. 
+
+rm -rf html 
+rm -rf frontend
+
+mv backend html
+
+cd html && pm2 start ecosystem.config.js
+
 echo "Setup complete. Reloading pm2 services..."
-pm2 start ecosystem.config.js
+
