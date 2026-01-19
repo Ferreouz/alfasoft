2b927dafcd4168701a6ed309fb6c2aaa8f76d2e7
ferr <g.f.guimaraes@hotmail.com>
2026-01-19 12:29:04 -0300

    fix BASE_URL & added response status

--- frontend/src/api/contacts.ts
+++ frontend/src/api/contacts.ts
@@ -1,67 +1,67 @@
 import type { Contact } from '@/types/Contact'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const BASE_URL = import.meta.env.VITE_API_BASE_URL + '/contacts';
 
 const api = {
   async getContacts(): Promise<Contact[]> {
-    const response = await fetch(`${API_BASE_URL}/contacts`)
+    const response = await fetch(`${BASE_URL}`);
 
     if (!response.ok) {
-      throw new Error('Failed to fetch contacts')
+      return Promise.reject({ status: response.status, message: 'Failed to fetch contacts' });
     }
 
     return response.json()
   },
 
   async getContactById(id: number): Promise<Contact> {
-    const response = await fetch(`${API_BASE_URL}/contacts/${id}`)
+    const response = await fetch(`${BASE_URL}/${id}`);
 
     if (!response.ok) {
-      throw new Error('Failed to fetch contact')
+      return Promise.reject({ status: response.status, message: 'Failed to fetch contact' });
     }
 
     return response.json()
   },
 
   async createContact(contact: Omit<Contact, 'id'>): Promise<Contact> {
-    const response = await fetch(`${API_BASE_URL}/contacts`, {
+    const response = await fetch(`${BASE_URL}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(contact)
-    })
+    });
 
     if (!response.ok) {
-      throw new Error('Failed to create contact')
+      return Promise.reject({ status: response.status, message: 'Failed to create contact' });
     }
 
     return response.json()
   },
 
   async updateContact(contact: Contact): Promise<Contact> {
-    const response = await fetch(`${API_BASE_URL}/contacts/${contact.id}`, {
+    const response = await fetch(`${BASE_URL}/${contact.id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(contact)
-    })
+    });
 
     if (!response.ok) {
-      throw new Error('Failed to update contact')
+      return Promise.reject({ status: response.status, message: 'Failed to update contact' });
     }
 
     return response.json()
   },
 
   async deleteContact(id: number): Promise<void> {
-    const response = await fetch(`${API_BASE_URL}/contacts/${id}`, {
+    const response = await fetch(`${BASE_URL}/${id}`, {
       method: 'DELETE'
-    })
+    });
 
     if (!response.ok) {
-      throw new Error('Failed to delete contact')
+      return Promise.reject({ status: response.status, message: 'Failed to delete contact' });
     }
   }
 }
