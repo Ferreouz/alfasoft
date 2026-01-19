4361e1c297a6da2e009fba4430fb15cbc1dac68e
ferr <g.f.guimaraes@hotmail.com>
2026-01-19 09:47:39 -0300

    added cors config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 
 import express from "express";
 import contactsRoutes from "./routes/contacts";
+import cors from 'cors';
+import corsOptions from "./config/cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use((req, _res, next) => {
 	console.log(`[${req.method}] url:: ${req.url}`);
